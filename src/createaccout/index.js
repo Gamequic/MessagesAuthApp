56a268a726e8d1d565b44473b1cae12f7541a827
Gamequic <demiancalleros0@gmail.com>
2023-04-08 23:52:15 -0600

    Faltan detalles de la ventana de chat

--- src/createaccout/index.js
+++ src/createaccout/index.js
@@ -1,12 +1,15 @@
 const showPasswordButton = document.getElementById('BTshow-password');
 const passwordInput = document.getElementById('password');
+const passwordConfirmInput = document.getElementById('password-confirm');
 
 showPasswordButton.addEventListener('click', function() {
     if (passwordInput.type === 'password') {
         passwordInput.type = 'text';
+        passwordConfirmInput.type = 'text';
         showPasswordButton.src = '../icons/hide.svg';
     } else {
         passwordInput.type = 'password';
+        passwordConfirmInput.type = 'password';
         showPasswordButton.src = '../icons/show.svg';
     }
 });
